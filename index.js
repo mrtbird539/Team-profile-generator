2c270013a1f6f4611a9fa0104b5896ce5f2371e0
Tyler Smith <mrtbird539@gmail.com>
2021-08-29 11:17:03 -0400

    Finished logic for adding team members.

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const Employee = require("./lib/employee");
 const Engineer = require("./lib/engineer");
 const Intern = require("./lib/intern");
 const Manager = require("./lib/manager");
-
 let isManager = false;
 let team = [];
 
@@ -35,25 +34,132 @@ const managerQuestions = [
     },
     {
         type: "list",
+        name: "addmem",
+        message: "Do you want to add more members?",
+        choices: [
+            "Yes",
+            "No",
+        ],
+    },
+]
+const newEmpQuestions = [
+    {
+        name: "name",
+        message: "What is the team member's name?",
+    },
+    {
+        name: "id",
+        message: "What is the team member's id?",
+    },
+    {
+        name: "email",
+        message: "What is the team member's email?",
+        // validate(input) {
+        //      if (emailValidator.validate(input)) {
+        //          return true;
+        //      } else {
+        //          return "Please enter a valid email address.";
+        //      }
+        // },
+    },
+    {
+        type: "list",
         name: "addRole",
-        message: "Which team member would you like to add?",
+        message: "What is the role of the member?",
         choices: [
             "Engineer",
             "Intern",
-            "I don't want to add anymore team members.",
+        ],
+    },
+]
+const engineerQues = [
+    {
+        name: "github",
+        message: "Enter team member's Github Account.",
+    },
+    {
+        type: "list",
+        name: "addmem",
+        message: "Do you want to add more members?",
+        choices: [
+            "Yes",
+            "No",
+        ],
+    },
+]
+const internQues = [
+    {
+        name: "school",
+        message: "Enter the interns school.",
+    },
+    {
+        type: "list",
+        name: "addmem",
+        message: "Do you want to add more members?",
+        choices: [
+            "Yes",
+            "No",
         ],
     },
 ]
 
+
 const newMember = function () {
+
     if (!isManager) {
         inquirer
             .prompt(managerQuestions)
             .then((answers) => {
-                team.push(answers)
+                const newTeamMem = new Manager(answers.name, answers.id, answer.email, answers.officeNumber)
+                team.push(newTeamMem)
                 console.log(team);
                 isManager = true;
+                if (answers.addmem == "Yes"){
+                newMember()
+                }
+                else {
+                }
             });
+    } else {
+        inquirer
+            .prompt(newEmpQuestions)
+            .then((answers) => {
+                let newEmpAns = answers
+                console.log(answers);
+                if (answers.addRole == "Intern") {
+                    inquirer
+                        .prompt(internQues)
+                        .then((internAnswers) => {
+                            let empAnswers = { ...newEmpAns, ...internAnswers }
+                            const newTeamMem = new Intern(empAnswers.name, empAnswers.id, empAnswers.email, empAnswers.school)
+                            team.push(newTeamMem);
+                            console.log(newTeamMem);
+                            if (empAnswers.addmem == "Yes"){
+                                newMember()
+                                }
+                                else {
+                                }
+                        })
+
+                } else if (answers.addRole == "Engineer") {
+                    inquirer
+                        .prompt(engineerQues)
+                        .then((engAnswers) => {
+                            let empAnswers = { ...newEmpAns, ...engAnswers }
+                            const newTeamMem = new Engineer(empAnswers.name, empAnswers.id, empAnswers.email, empAnswers.github)
+                            console.log(newTeamMem)
+                            team.push(newTeamMem);
+                            console.log(team);
+                            if (empAnswers.addmem == "Yes"){
+                                newMember()
+                                }
+                                else {
+                                }
+                        })
+                } else {
+                    return;
+                }
+            })
     }
 }
 newMember();
